3ec3af8b089213dcbad6892285513cc6d7df24df
jhonatan <jhonmnp6@gmail.com>
2024-02-23 12:08:22 -0300

    jhon

---
+++ javascript/array e objeto/exercicio2.js
@@ -0,0 +1,8 @@
+let elementos = ["banana","maçã","uva", 'abc'];
+let primeiraLetra1 = elementos[0].slice(0,1);
+let primeiraLetra2 = elementos[1].slice(0,1);
+let primeiraLetra3 = elementos[2].slice(0,1);
+console.log("Primeira letra do primeiro elemento", primeiraLetra1)
+console.log("Primeira letra do segundo elemento", primeiraLetra2)
+console.log("Primeira letra do terceiro elemento", primeiraLetra3)
+
